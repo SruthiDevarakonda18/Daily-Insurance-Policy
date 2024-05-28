dd7eec862a6cbc5ab5dae3a78f27917869fa428d
SruthiDevarakonda18 <81910652+SruthiDevarakonda18@users.noreply.github.com>
2024-05-28 19:22:48 +0530

    Update PurchasePolicy.js

--- dailyinsurance/src/Components/PurchasePolicy.js
+++ dailyinsurance/src/Components/PurchasePolicy.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react'
+import React,{ useState,useEffect } from 'react'
 import { useForm } from 'react-hook-form';
 import { NavLink } from 'react-router-dom';
 import { ClientLoginContextObj } from '../Context/ClientLoginContext';
@@ -12,23 +12,34 @@ function PurchasePolicy() {
     let {register,handleSubmit,formState: { errors }}= useForm();
     let {userLoginStatus,currentUser}=useContext(ClientLoginContextObj)
 
-  //const policyData,setPolicyData  = useState([])
-    //UseEffect 
-
+    const [policyData, setPolicyData] = useState([]);
+    useEffect(() => {
+      const fetchPolicyData = async () => {
+        
+          const response = await axios.get("http://localhost:4000/policyClaimTable");
+          setPolicyData(response.data);
+       
+      };
+  
+      fetchPolicyData();
+    }, []); 
 async function HandlePolicy(policyObj){
     console.log(policyObj);
-    //const selectedPolicy = policyData.filter((item) => item.policyId === policyObj.policyId)
+    const selectedPolicy = policyData.filter((item) => item.policyId === policyObj.policyId)
     const updatedUser = ({
       userId : currentUser.username,
       premium : policyObj.premium,
-      policyname:policyObj.policyname,
-      // maxCost : selectedPolicy.maxCost,
-      //DateOfPurchase : new Date().toString();
-      //ClaimAmount : "",
-      //ClaimStatus : false,
-      //DateTimeOfClaim : ""
+      policyName:selectedPolicy[0].policyName,
+      maxCost : selectedPolicy[0].maxcost,
+      DateOfPurchase : new Date().toString(),
+      ClaimAmount : "",
+      ClaimStatus : false,
+      DateTimeOfClaim : ""
     })
     console.log(updatedUser);
+    console.log(selectedPolicy)
+    console.log(policyObj)
+    
     await axios.post("http://localhost:4000/policyClaimTable",updatedUser);
   navigate('/process-claim')
 }
@@ -46,17 +57,20 @@ async function HandlePolicy(policyObj){
             <h1 className="mb-3 fs-3 text-center ">Purchase your Daily Insurance Policy</h1>
         
             <div className='mb-4'>
-                 <select  {...register('policyname',{required:true})} className = "form-control">
+                 <select  {...register('policyId',{required:true})} className = "form-control">
               <option value="">Policy name</option>
-{/* 
-            policyData.map((item)=>{
-              return  <option value={item.POL1}>{item.policyName}</option>
-              
-            })
-               */}
-              <option value="Daily Accidental Coverage policy">Daily Accidental Coverage policy </option>
+{
+            
+              policyData.map((item) => {
+                return  <option value={item.policyId}>{item.policyName}</option>
+              })
+            }
+            
+            
+              {/* <option value="Daily Accidental Coverage policy">Daily Accidental Coverage policy </option> */}
           </select> 
-           {errors.policyname?.type==='required' && <p className='fs-8' >please select from the list</p> }
+          
+           {errors.policyId?.type==='required' && <p className='fs-8' >please select from the list</p> }
 </div>
          
           <div className='mb-4'>
@@ -73,3 +87,97 @@ async function HandlePolicy(policyObj){
 }
 
 export default PurchasePolicy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React,{ useState } from 'react'
+// import { useForm } from 'react-hook-form';
+// import { NavLink } from 'react-router-dom';
+// import { ClientLoginContextObj } from '../Context/ClientLoginContext';
+// import { useContext } from 'react';
+// import { Link } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
+// import axios from 'axios'
+
+// function PurchasePolicy() {
+//   let navigate = useNavigate();
+//     let {register,handleSubmit,formState: { errors }}= useForm();
+//     let {userLoginStatus,currentUser}=useContext(ClientLoginContextObj)
+
+//   //const policyData,setPolicyData  = useState([])
+//     //UseEffect 
+
+// async function HandlePolicy(policyObj){
+//     console.log(policyObj);
+//     //const selectedPolicy = policyData.filter((item) => item.policyId === policyObj.policyId)
+//     const updatedUser = ({
+//       userId : currentUser.username,
+//       premium : policyObj.premium,
+//       policyname:policyObj.policyname,
+//       // maxCost : selectedPolicy.maxCost,
+//       //DateOfPurchase : new Date().toString();
+//       //ClaimAmount : "",
+//       //ClaimStatus : false,
+//       //DateTimeOfClaim : ""
+//     })
+//     console.log(updatedUser);
+//     await axios.post("http://localhost:4000/policyClaimTable",updatedUser);
+//   navigate('/process-claim')
+// }
+//   return (
+//     <>
+//     {userLoginStatus === false?
+//       (
+//         <div>
+//         <h3>Ouch! you have not logged in!</h3>
+//         <Link to="/clientLogin" >Click here to login!</Link>
+//         </div>
+//       ):(
+//     <form className=' w-25  mx-auto mt-5'  onSubmit={handleSubmit(HandlePolicy)} style={{minHeight:'80vh'}}>
+        
+//             <h1 className="mb-3 fs-3 text-center ">Purchase your Daily Insurance Policy</h1>
+        
+//             <div className='mb-4'>
+//                  <select  {...register('policyname',{required:true})} className = "form-control">
+//               <option value="">Policy name</option>
+// {/* 
+//             policyData.map((item)=>{
+//               return  <option value={item.POL1}>{item.policyName}</option>
+              
+//             })
+//                */}
+//               <option value="Daily Accidental Coverage policy">Daily Accidental Coverage policy </option>
+//           </select> 
+//            {errors.policyname?.type==='required' && <p className='fs-8' >please select from the list</p> }
+// </div>
+         
+//           <div className='mb-4'>
+//             <label  className="form-label">Premium</label>
+//             <input type='number' className='form-control' {...register('premium',{required:true})}/>
+//             {errors.premium?.type==='required' && <p>premium is required</p> }
+//           </div>
+//           <br/>
+//          <button type='submit' className='btn btn-primary  float-end'>Next</button>
+//     </form>
+//       )}
+//     </>
+//   )
+// }
+
+// export default PurchasePolicy
